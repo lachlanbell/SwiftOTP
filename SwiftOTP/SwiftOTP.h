88f75c1322cd1215343456e30d963376bb0bc7aa
Lachlan Bell <me@lachlanbell.com.au>
2018-01-12 16:59:38 +0800

    Initial Commit

---
+++ SwiftOTP/SwiftOTP.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOTP.h
+//  SwiftOTP
+//
+//  Created by Lachlan Bell on 12/1/18.
+//  Copyright © 2018 Lachlan Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOTP.
+FOUNDATION_EXPORT double SwiftOTPVersionNumber;
+
+//! Project version string for SwiftOTP.
+FOUNDATION_EXPORT const unsigned char SwiftOTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOTP/PublicHeader.h>
+
+
