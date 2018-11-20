297fe4340bfe015188fb4805c81a094cccbc9cca
Lachlan Bell <me@lachlanbell.com.au>
2018-11-20 12:56:08 +0800

    Add license information to (more) file headers

--- SwiftOTP/Generator.swift
+++ SwiftOTP/Generator.swift
@@ -3,7 +3,7 @@
 //  SwiftOTP
 //
 //  Created by Lachlan Bell on 12/1/18.
-//	Copyright © 2018 Lachlan Bell. All rights reserved.
+//  Copyright © 2018 Lachlan Bell. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
