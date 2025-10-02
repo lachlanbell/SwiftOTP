78d37d473a481d1c60df7bbdffdc816fec79ca57
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-10-02 13:03:00 +0900

    refactor

--- Package.swift
+++ Package.swift
@@ -8,14 +8,12 @@ let package = Package(
     ],
     products: [
         .library(name: "SwiftOTP", targets: ["SwiftOTP"]),
-        .library(name: "SwiftOTP-Dynamic", type: .dynamic, targets: ["SwiftOTP"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" 
-..< "4.0.0")
+		.package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "SwiftOTP", dependencies: ["Crypto"], path: "SwiftOTP/"),
+        .target(name: "SwiftOTP", dependencies: ["Crypto"]),
         .testTarget(name: "SwiftOTPTests", dependencies: ["SwiftOTP"])
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-
-import SwiftOTPTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftOTPTests.__allTests()
-
-XCTMain(tests)
