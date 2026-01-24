73cbe933fa513acca4166fb70d40d60b943212f3
Piotr Merski <merskip@gmail.com>
2026-01-24 11:09:06 +0100

    Fixed typo

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "SwiftOTP-Dynamic", type: .dynamic, targets: ["SwiftOTP"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0" ..< "5.0.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0")
     ],
     targets: [
         .target(name: "SwiftOTP", dependencies: ["Crypto"], path: "SwiftOTP/"),
